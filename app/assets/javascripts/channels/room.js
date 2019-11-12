173132d2fc5fdcf02ee0cbd37656ea99c22ab363
imaizumi <metallicaslayer2002@yahoo.co.jp>
2019-11-12 17:39:38 +0900

    バグの修正

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,5 +1,6 @@
 App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
+    console.log('connected')
     // Called when the subscription is ready for use on the server
   },
 
@@ -8,23 +9,22 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
   },
 
   received: function(message) {
-    const messages = document.getElementById('messages');
-    messages.innerHTML = '<p>${message}</p>';
+    const messages = document.getElementById('messages')
+    messages.innerHTML += message
     // Called when there's incoming data on the websocket for this channel
   },
 
   speak: function(content) {
-    return this.perform('speak',{message: content});
+    return this.perform('speak', {message: content});
   }
 });
 
-document.addEventListener('DOMContentLoaded',function(){
-  const input = document.getElementById('chat-input');
-  const button = document.getElementById('button');
-  button.addEventListener ('click',function(){
-    const content = input.value 
-    App.room.speak(content);
-    input.value = '';
+document.addEventListener('DOMContentLoaded', function(){
+  const input = document.getElementById('chat-input')
+  const button = document.getElementById('button')
+  button.addEventListener('click', function(){
+    const content = input.value
+    App.room.speak(content)
+    input.value = ''
   })
-
 })
